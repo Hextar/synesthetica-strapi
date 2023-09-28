617e5300ce81bb256ce7bba8641268a545a28fe0
Stefano Salidu <st.salidu@gmail.com>
2023-09-28 12:16:57 +0200

    chore: added plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,33 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  publisher: {
+    enabled: true,
+  },
+  "schemas-to-ts": {
+    enabled: true,
+  },
+  seo: {
+    enabled: env("NODE_ENV") === "production",
+  },
+  tinymce: {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
+  },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
     },
-  });
+  },
+});
